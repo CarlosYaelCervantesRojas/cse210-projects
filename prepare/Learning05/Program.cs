6e92bd004d15e77d869bacc5052d1cabd37c3dc1
CarlosYaelCervantesRojas <carlos.yael.cervantes.rojas@gmail.com>
2024-02-05 16:34:54 -0600

    W05 Prepare: Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("red", 5);
+        Rectangle rectangle = new Rectangle("yellow", 10, 5);
+        Circle circle = new Circle("white", 10);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
